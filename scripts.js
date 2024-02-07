4fc265d02711d9c208ce54b560c9b87af1749ea3
Anna <anna.cazzanelli1@gmail.com>
2024-02-07 13:51:57 +0100

    molti cambiamenti

--- scripts.js
+++ scripts.js
@@ -9,3 +9,19 @@ const toggleMenu = () => {
 document.addEventListener('DOMContentLoaded', () =>{
   toggle.addEventListener('click', toggleMenu)
 })
+
+const setupReadMore = () => {
+  console.log("loaded")
+  const list = document.querySelectorAll(".accordion")
+  console.log(list)
+  list.forEach(function (accordion, index) {
+    const link = accordion.querySelector('a.more')
+    const p = accordion.querySelector('p.more')
+    console.log(p);
+    link.addEventListener('click', () => {
+      p.style.display = p.style.display == "block" ? "none" : "block";
+    })
+  })
+}
+
+document.addEventListener("DOMContentLoaded", setupReadMore);
